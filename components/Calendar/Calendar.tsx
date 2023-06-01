4ec144b5c8ba8bfdc662dd0fbfcf34cc25ec0b34
Carmilla Mina Storm <carmillam@icloud.com>
2023-06-01 23:03:09 +0200

    feat: beautify calendar

--- components/Calendar/Calendar.tsx
+++ components/Calendar/Calendar.tsx
@@ -1,12 +1,16 @@
+import ArrowLeftIcon from 'assets/arrow-left.svg';
+import ArrowRightIcon from 'assets/arrow-right.svg';
+import CalendarIcon from 'assets/calendar.svg';
 import { calendarEventsAtom } from 'atoms/calendar';
 import dayjs from 'dayjs';
 import svLocale from 'dayjs/locale/sv';
 import dayjsUtc from 'dayjs/plugin/utc';
 import dayjsWeekday from 'dayjs/plugin/weekday';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import {
   Calendar as BigCalendar,
-  dayjsLocalizer
+  dayjsLocalizer,
+  ToolbarProps
 } from 'react-big-calendar';
 import 'react-big-calendar/lib/addons/dragAndDrop/styles.scss';
 import 'react-big-calendar/lib/sass/styles.scss';
@@ -14,8 +18,89 @@ import { useRecoilValue } from 'recoil';
 import { RRule } from 'rrule';
 import styled from 'styled-components';
 
+import { colors } from '../../styles/global';
+import Button from '../Button/Button';
+
 const StyledDiv = styled.div`
-  background-color: white;
+  .rbc-time-view, .rbc-month-view, .rbc-time-view, .rbc-agenda-view {
+    background-color: ${colors.componentBackground};
+    border-color: ${colors.primaryColor};
+    border-radius: 4px;
+  }
+  
+  .rbc-calendar {
+    height: calc(100svh - 4.1rem);
+    box-sizing: border-box;
+    padding: .5rem;
+    width: 100%;
+  }
+  
+  .rbc-today {
+    background-color: ${colors.selected};
+  }
+  
+  .rbc-time-gutter, .rbc-time-header-cell {
+    background-color: ${colors.clickableBackground};
+  }
+  
+  .rbc-calendar, .rbc-header, .rbc-events-container, .rbc-time-header-content, .rbc-time-header, .rbc-time-content, .rbc-timeslot-group, .rbc-day-bg {
+    border-color: ${colors.primaryColor};
+  }
+  
+  .rbc-time-slot {
+    border-color: ${colors.active};
+  }
+  
+  .rbc-event {
+    background-color: ${colors.brightColor};
+    color: ${colors.primaryColor};
+    border: 1px solid;
+    box-shadow: 0 0 5px 0 rgba(0,0,0,0.5);
+  }
+`;
+
+const ToolbarButton = styled(Button)`
+  background-color: inherit;
+  color: ${colors.brightColor};
+  border-bottom: none;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+  
+  svg {
+    width: 1rem;
+    height: 1rem;
+  }
+`;
+
+const ToolbarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  grid-gap: 2rem;
+  position: relative;
+  padding: 0 1rem;
+`;
+
+const NavContainer = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  align-items: center;
+  grid-gap: .5rem;
+`;
+
+const DateSpan = styled.span`
+  text-align: center;
+  position: absolute;
+  right: 0;
+  left: 0;
+  top: -1.7rem;
+  color: ${colors.brightColor};
+  background-color: ${colors.primaryTransBackground};
+  font-size: 1.1rem;
+  z-index: 2;
+  width: fit-content;
+  margin: 0 auto;
+  padding: .1rem;
 `;
 
 dayjs.extend(dayjsWeekday);
@@ -24,6 +109,28 @@ dayjs.extend(dayjsUtc);
 dayjs.locale('sv', svLocale);
 
 const localizer = dayjsLocalizer(dayjs);
+
+const Toolbar: React.FC<ToolbarProps> = ({
+  label, onNavigate, onView, ...otherProps
+}) => {
+  console.log('toolbar', otherProps);
+
+  return (
+    <ToolbarContainer>
+      <NavContainer>
+        <ToolbarButton onClick={() => onNavigate('PREV')} aria-label="Previous"><ArrowLeftIcon /></ToolbarButton>
+        <ToolbarButton onClick={() => onNavigate('TODAY')} aria-label="Today"><CalendarIcon /></ToolbarButton>
+        <ToolbarButton onClick={() => onNavigate('NEXT')} aria-label="Next"><ArrowRightIcon /></ToolbarButton>
+      </NavContainer>
+      <DateSpan>{label}</DateSpan>
+      <NavContainer>
+        <ToolbarButton onClick={() => onView('week')} aria-label="Week"><CalendarIcon /></ToolbarButton>
+        <ToolbarButton onClick={() => onView('day')} aria-label="Day"><CalendarIcon /></ToolbarButton>
+        <ToolbarButton onClick={() => onView('agenda')} aria-label="Agenda"><CalendarIcon /></ToolbarButton>
+      </NavContainer>
+    </ToolbarContainer>
+  );
+};
 
 const Calendar = () => {
   const icalSource = useRecoilValue(calendarEventsAtom);
@@ -59,6 +166,10 @@ const Calendar = () => {
   return (
     <StyledDiv>
       <BigCalendar
+        selectable
+        components={{
+          toolbar: Toolbar,
+        }}
         culture="sv"
         defaultView="week"
         onRangeChange={(range) => {
@@ -76,20 +187,18 @@ const Calendar = () => {
         localizer={localizer}
         startAccessor="start"
         endAccessor="end"
-        resources={[...([...icalSource.values()].reduce((resources, event) => {
-          if (event.location) {
-            resources.set(event.location, {
-              resourceId: event.location,
-              resourceName: event.location,
-            });
-          }
-
-          return resources;
-        }, new Map()).values())]}
-        resourceAccessor="location"
-        resourceIdAccessor="resourceId"
-        resourceTitleAccessor="resourceName"
-        style={{ height: 'calc(100vh - 4.1rem)', maxWidth: '100vw' }}
+        // resources={[...([...icalSource.values()].reduce((resources, event) => {
+        //   if (event.location) {
+        //     resources.set(event.location, {
+        //       resourceId: event.location,
+        //       resourceName: event.location,
+        //     });
+        //   }
+        //
+        //   return resources;
+        // }, new Map()).values())]}
+        // resourceIdAccessor="resourceId"
+        // resourceTitleAccessor="resourceName"
       />
     </StyledDiv>
   );
