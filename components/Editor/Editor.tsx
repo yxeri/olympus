ece1d6336817085f3d868d152268403203b2f7a8
Carmilla Mina Storm <carmillam@icloud.com>
2023-06-10 15:42:24 +0200

    feat: make document pages printable

--- components/Editor/Editor.tsx
+++ components/Editor/Editor.tsx
@@ -20,17 +20,14 @@ const StyledDiv = styled.div`
   
   .ck-editor__editable {
     /* Set the dimensions of the "page". */
-    max-width: 15.8cm;
-    min-height: 21cm;
-
-    /* Keep the "page" off the boundaries of the container. */
-    padding: 1cm 2cm 2cm;
+    max-width: 18.6cm;
+    min-height: 25cm;
 
     border: 1px hsl( 0,0%,82.7% ) solid;
     background: white;
 
     /* The "page" should cast a slight shadow (3D illusion). */
-    box-shadow: 0 0 5px hsla( 0,0%,0%,.1 );
+    box-shadow: 0 0 5px hsla(0, 0%, 0%, .1);
 
     /* Center the "page". */
     margin: 0 auto;
@@ -54,7 +51,7 @@ const Editor = () => {
   }, []);
 
   return (
-    <StyledDiv>
+    <StyledDiv className="inner-editor-container">
       {DocumentEditor && CkEditor && (
         <CkEditor
           editor={DocumentEditor}
