f9f0d7c6738a0c13a81faa6fd633a8a74c599c89
Carmilla Mina Storm <carmillam@icloud.com>
2024-03-28 15:26:04 +0100

    ci: fixes

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -42,8 +42,8 @@ export default async function handler(
     }
 
     const supabaseClient = createClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
       {
         auth: {
           autoRefreshToken: false,
